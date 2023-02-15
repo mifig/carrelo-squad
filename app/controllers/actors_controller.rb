d17c40d544fd2cabdb669ab435a1cefa334c3426
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2023-02-15 10:11:34 +0000

    Solved adding english of bio, hair and eyes in form params on the controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -63,6 +63,6 @@ class ActorsController < ApplicationController
   end
 
   def actor_params
-    params.require(:actor).permit(:first_name, :last_name, :email, :phone_number, :bio, :height, :hair, :eyes, :min_age, :max_age, :primary_photo, :pt_cv, :en_cv, photos: [])
+    params.require(:actor).permit(:first_name, :last_name, :email, :phone_number, :bio, :bio_en, :height, :hair, :hair_en, :eyes, :eyes_en, :min_age, :max_age, :primary_photo, :pt_cv, :en_cv, photos: [])
   end
 end
