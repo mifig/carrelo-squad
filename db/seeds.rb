6b480004fc49bd0c3f94f9eda4e7866c53486995
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2023-02-22 13:51:08 +0000

    Resized index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,14 @@ puts "   DB cleaned!"
 
 # CREATING ADMIN USER:
 puts "2. Creating Admin User..."
-User.create!(email: "rscarrelo@gmail.com", password: "111111", admin: true)
+User.create!(
+      email: "rscarrelo@gmail.com", 
+      password: "111111", 
+      bio: "Bio [PT]", 
+      bio_en: "Bio [EN]", 
+      about: "About [PT]", 
+      about_en: "About [EN]",
+      admin: true)
 puts "   Admin User created!"
 
 # CREATING SOCIAL NETWORKS:
@@ -99,10 +106,10 @@ actor1_imdbvideo = Link.create(social: imdb, url: "https://www.imdb.com/video/vi
 actor1_youtubevideo = Link.create(social: youtube, url: "6hFqd62GICI", actor: actor1, video: true)
 actor1_vimeovideo = Link.create(social: vimeo, url: "https://player.vimeo.com/video/768031556?h=02cbbdc5d5", actor: actor1, video: true)
 
-# pt_cv = File.open("/Users/miguelfigueiredo/Desktop/00_Docs/01_JobApplicationDocs/MotivationLetter_PostGrad_DataScience_Tecnico.pdf")
-# en_cv = File.open("/Users/miguelfigueiredo/Desktop/00_Docs/01_JobApplicationDocs/MotivationLetter_PostGrad_DataScience_Tecnico.pdf")
-# actor1.pt_cv.attach(io: pt_cv, filename: "#{actor1.first_name}-#{actor1.last_name}-pt-cv", content_type: "application/pdf")
-# actor1.en_cv.attach(io: en_cv, filename: "#{actor1.first_name}-#{actor1.last_name}-en-cv", content_type: "application/pdf")
+pt_cv = File.open("/Users/miguelfigueiredo/Desktop/00_Docs/01_JobApplicationDocs/MotivationLetter_PostGrad_DataScience_Tecnico.pdf")
+en_cv = File.open("/Users/miguelfigueiredo/Desktop/00_Docs/01_JobApplicationDocs/MotivationLetter_PostGrad_DataScience_Tecnico.pdf")
+actor1.pt_cv.attach(io: pt_cv, filename: "#{actor1.first_name}-#{actor1.last_name}-pt-cv", content_type: "application/pdf")
+actor1.en_cv.attach(io: en_cv, filename: "#{actor1.first_name}-#{actor1.last_name}-en-cv", content_type: "application/pdf")
 
 actor1.save
 
